0dd38fb1e997185880d7402c5d3ddd5a3878442b
pyojhong <zerribin@daum.net>
2026-09-07 13:52:00 +0900

    todo list 추가

--- javascript/greetings.js
+++ javascript/greetings.js
@@ -15,7 +15,7 @@ function onLoginSubmit(event) {
 function paintGreetings() {
     const username = localStorage.getItem(USERNAME_KEY);
     greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello, ${username}`
+    greeting.innerText = `안녕하세요, ${username}`
 }
 
 
---
+++ javascript/todo.js
@@ -0,0 +1,29 @@
+const toDoForm = document.querySelector('#todo-form');
+const toDoInput = toDoForm.querySelector('input');
+const toDoList = document.querySelector('#todo-list');
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newTodo) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.innerText = newTodo;
+    const button = document.createElement('button');
+    button.innerText = "❌";
+    button.addEventListener('click', deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+    paintToDo(newToDo);
+}
+
+toDoForm.addEventListener('submit', handleToDoSubmit);
