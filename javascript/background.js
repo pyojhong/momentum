e2733c8045b96239f38ae64877f7aa394a61ba9a
pyojhong <zerribin@daum.net>
2026-09-07 13:09:15 +0900

    백그라운드 반영

--- javascript/background.js
+++ javascript/background.js
@@ -10,4 +10,8 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-console.log(chosenImage);
+// HTML 태그 생성
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
