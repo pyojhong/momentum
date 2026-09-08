f59513750efef9ea238db7d81d7a497c3b1a08d0
pyojhong <zerribin@daum.net>
2026-09-08 17:48:08 +0900

    날씨 업데이트 및 CSS 반영

--- javascript/weather.js
+++ javascript/weather.js
@@ -6,12 +6,16 @@ function onGeoOk(position) {
     console.log(`현재 위치:`, lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector('#weather span:first-child');
-        const city = document.querySelector('#weather span:nth-child(2)');
-        const celcius = document.querySelector('#weather span:last-child');
+        const weatherIcon = document.querySelector('#weather-icon');
+        const city = document.querySelector('#city');
+        const temp = document.querySelector('#temp');
+
+        const iconCode = data.weather[0].icon;
+        weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+        weatherIcon.classList.remove('hidden');
+
         city.innerText = data.name;
-        weather.innerText = data.weather[0].main;
-        celcius.innerText = data.main.temp;
+        temp.innerText = `${data.main.temp}°C`;
     });
 }
 function onGeoError() {
